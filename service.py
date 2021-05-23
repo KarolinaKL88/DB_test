ae2a2e5052efcf200e940d56e6e657f2aab29c21
KarolinaKL <karolina.krawczak@gmail.com>
2021-05-23 15:51:40 +0200

    Add functions

--- service.py
+++ service.py
@@ -31,3 +31,62 @@ def get_authors_by_publishers(session, ascending=True):
             .group_by(Publisher.name)
             .order_by(direction("total_authors"))
     )
+
+
+def get_authors(session):
+    return session.query(Author).order_by(Author.last_name).all()
+
+
+#najbardziej skomplikowane - dodanie nowej książki
+
+def add_new_book(session, author_name, book_title, publisher_name):
+    first_name, _, last_name = author_name.partition(" ") #dzielimy po spacji
+    book = session.query(Book) \
+        .join(Author) \
+        .filter(Book.title == book_title) \
+        .filter(
+            and_(
+                Author.first_name == first_name, Author.last_name == last_name
+            )
+        ) \
+        .filter(Book.publishers.any(Publisher.name == publisher_name)) \
+        .one_or_none()
+
+    if book is not None:
+        return
+
+    book = session.query(Book) \
+        .join(Author) \
+        .filter(Book.title == book_title) \
+        .filter(
+        and_(
+            Author.first_name == first_name, Author.last_name == last_name
+        )
+    )   .one_or_none()
+
+    if book is None:
+        book = Book(title=book_title)
+
+    author = session.query(Author).filter (
+        and_(
+            Author.first_name == first_name, Author.last_name == last_name
+        )
+    ).one_or_more()
+
+    if author is None:
+        author = Author(first_name=first_name, last_name=last_name)
+        session.add(author)
+
+    publisher = session.query(Publisher) \
+        .filter(Publisher.name == publisher_name) \
+        .one_or_more()
+
+    if publisher is None:
+        publisher = Publisher(name=publisher_name)
+        session.add(publisher)
+
+    book.author = author
+    book.publisher = publisher
+    session.add(book)
+
+    session.commit()
